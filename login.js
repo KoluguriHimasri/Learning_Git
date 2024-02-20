3aeb5091dd88af12326c4fbca7166b98182a1344
unknown <himasrikoluguri@gmail.com>
2024-02-20 15:30:52 +0530

    js file added

--- login.js
+++ login.js
@@ -1,11 +1,10 @@
+document.getElementById("loginForm").addEventListener("submit", function(event){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
 
-        document.getElementById("loginForm").addEventListener("submit", function(event){
-            var username = document.getElementById("username").value;
-            var password = document.getElementById("password").value;
-
-            if (username === "" || password === "") {
-                alert("Please enter both username and password.");
-                
-                event.preventDefault(); // Prevent form submission
-            }
-        });
+    if (username === "" || password === "") {
+        alert("Please enter both username and password.");
+        
+        event.preventDefault(); // Prevent form submission
+    }
+});
